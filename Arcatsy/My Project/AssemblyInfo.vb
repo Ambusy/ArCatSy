2899a0e9907f6228abde144ab4f816c8e67fde1c
Ambusy <Ambusy@users.noreply.github.com>
2018-11-18 11:45:44 +0200

    Add files via upload

---
+++ Arcatsy/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' Le informazioni generali relative a un assembly sono controllate dal seguente 
+' insieme di attributi. Per modificare le informazioni associate a un assembly
+' è necessario modificare i valori di questi attributi.
+
+' Controllare i valori degli attributi dell'assembly
+
+<Assembly: AssemblyTitle("ArCatSy")>
+<Assembly: AssemblyDescription("Archives Cataloguing System")>
+<Assembly: AssemblyCompany("AMBusy")>
+<Assembly: AssemblyProduct("SQLiteTest")>
+<Assembly: AssemblyCopyright("Copyright ©  2018")>
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'Se il progetto viene esposto a COM, il GUID seguente verrà utilizzato come ID della libreria dei tipi
+<Assembly: Guid("c4375c34-3ee7-41a3-a90e-f6c8bab59921")> 
+
+' Le informazioni sulla versione di un assembly sono costituite dai seguenti quattro valori:
+'
+'      Numero di versione principale
+'      Numero di versione secondario 
+'      Numero build
+'      Revisione
+'
+' È possibile specificare tutti i valori oppure impostare valori predefiniti per i numeri relativi alla revisione e alla build 
+' utilizzando l'asterisco (*) come descritto di seguito:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.1")> 
+<Assembly: AssemblyFileVersion("1.0.0.1")> 
